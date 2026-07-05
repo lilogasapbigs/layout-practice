0cafd5e179cb53804216ac86d959a43bc8a66bf2
asap bigs <bigs@MacBook-Air-asap.local>
2026-07-06 05:56:01 +1100

    сделал live input

--- js_tasks/block-7-events/script.js
+++ js_tasks/block-7-events/script.js
@@ -5,3 +5,7 @@ document.querySelector('#counter-button')?.addEventListener('click', () => {
     counter += 1;
     document.querySelector('#counter-value').textContent = counter;
 });
+
+document.querySelector('#live-input')?.addEventListener('input', (event) => {
+    document.querySelector('#live-output').textContent = event.target.value || 'Тут будет текст';
+});
