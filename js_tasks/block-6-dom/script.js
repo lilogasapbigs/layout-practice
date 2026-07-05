afaf13c5dacd26b039537d9b55432c14c3f5fe16
asap bigs <bigs@MacBook-Air-asap.local>
2026-07-06 05:55:59 +1100

    сделал активную карточку

--- js_tasks/block-6-dom/script.js
+++ js_tasks/block-6-dom/script.js
@@ -4,3 +4,7 @@ document.querySelector('#change-title-button')?.addEventListener('click', () => 
     const title = document.querySelector('#change-title');
     if (title) title.textContent = 'Текст заголовка изменился';
 });
+
+document.querySelector('#active-card-button')?.addEventListener('click', () => {
+    document.querySelector('#active-card')?.classList.toggle('active');
+});
