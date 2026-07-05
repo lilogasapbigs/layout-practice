73cb4f0006cb49614619c8e97187614b43a0256d
asap bigs <bigs@MacBook-Air-asap.local>
2026-07-06 05:55:59 +1100

    добавил элементы списка

--- js_tasks/block-6-dom/script.js
+++ js_tasks/block-6-dom/script.js
@@ -8,3 +8,13 @@ document.querySelector('#change-title-button')?.addEventListener('click', () => 
 document.querySelector('#active-card-button')?.addEventListener('click', () => {
     document.querySelector('#active-card')?.classList.toggle('active');
 });
+
+document.querySelector('#create-item-button')?.addEventListener('click', () => {
+    const input = document.querySelector('#create-item-input');
+    const text = input.value.trim();
+    if (!text) return;
+    const li = document.createElement('li');
+    li.textContent = text;
+    document.querySelector('#created-list').append(li);
+    input.value = '';
+});
